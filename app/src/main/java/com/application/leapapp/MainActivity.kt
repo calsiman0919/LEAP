96b8a590f050bade715e89fc74f3040798ee10e9
calsiman0919 <clevir0919@gmail.com>
2022-11-16 17:01:20 +0800

    2nd

--- app/src/main/java/com/application/leapapp/MainActivity.kt
+++ app/src/main/java/com/application/leapapp/MainActivity.kt
@@ -1,20 +1,27 @@
 package com.application.leapapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.min
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mAuth: FirebaseAuth
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        mAuth = FirebaseAuth.getInstance()
 
     }
 
@@ -22,9 +29,18 @@ class MainActivity : AppCompatActivity() {
         menuInflater.inflate(R.menu.main_menu, menu)
         return true
 
-        mAuth = FirebaseAuth.getInstance()
 
 
+
+
+    }
+
+    fun button(view: View) {
+        mAuth.signOut()
+        val intent = Intent(this, LogIn::class.java)
+        startActivity(intent)
+        finish()
+        Toast.makeText(this, "Logout Successful" , Toast.LENGTH_SHORT).show()
     }
 
 }
