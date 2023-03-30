f596b827ae85f05e85c26319a96d619a4bf07cb4
calsiman0919 <clevir0919@gmail.com>
2023-03-31 07:08:47 +0800

    contact added

--- app/src/main/java/com/application/leapapp/adapter/ViewPagerAdapter.kt
+++ app/src/main/java/com/application/leapapp/adapter/ViewPagerAdapter.kt
@@ -23,7 +23,7 @@ class ViewPagerAdapter(
     }
 
     companion object{
-        val TAB_TITLES = arrayOf("Learn","Find","Contacts")
+        val TAB_TITLES = arrayOf("Learn","Find","Contact")
     }
 
 }
--- app/src/main/java/com/application/leapapp/ui/StatusFragment.kt
+++ app/src/main/java/com/application/leapapp/ui/StatusFragment.kt
@@ -35,7 +35,7 @@ class StatusFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contacts, container, false)
+        return inflater.inflate(R.layout.fragment_contact, container, false)
     }
 
     companion object {
